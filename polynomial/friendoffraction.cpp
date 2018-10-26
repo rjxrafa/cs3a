1d23b19362a4ee4b664cef2f64aaf40e1b91d4d7
Rafa <rafa.betita@gmail.com>
2018-10-26 13:40:43 -0700

    should be able to input all cases of doubles now

--- polynomial/friendoffraction.cpp
+++ polynomial/friendoffraction.cpp
@@ -4,7 +4,7 @@ std::ostream& operator<<(std::ostream& out, const fraction &frac)
 {
     out<<frac.num;
     if(frac.denom != 1)
-         out<<"/"<<frac.denom;
+        out<<"/"<<frac.denom;
     return out;
 }
 
@@ -13,7 +13,7 @@ std::istream& operator>>(std::istream& in, fraction &frac)
     char junk;
     std::stringstream ss;
     std::string possibleFraction;
-    if(&in == &std::cin) //This means that the program is reading from the console
+    if(&in == &std::cin) // This means that the program is reading from the console
     {
         if(in>>possibleFraction)
         {
@@ -21,16 +21,38 @@ std::istream& operator>>(std::istream& in, fraction &frac)
             ss>>frac;
         }
     }
-    else//Let's assume everything else is a file (for now)
+    else // Let's assume everything else is a file (for now)
     {
         if(in>>frac.num)
         {
-            if(in.peek() == '/')
-               in>>junk>>frac.denom;
-            frac.reduce();
+            if (in.peek() == '/')
+            {
+                in >> junk >> frac.denom;
+                frac.reduce();
+            }
+
+            if (in.peek() == '.') // Example "0.5"
+            {
+                double temp;
+                in >> temp;
+                temp = temp + frac.num;
+                fraction a(temp);
+                frac = a;
+                frac.reduce();
+            }
         }
         else
-            std::cout<<"Hit End of File"<<std::endl;
+        {
+            in.clear();
+            if (in.peek() == '.') // Example ".5"
+            {
+                double temp;
+                in >> temp;
+                fraction a(temp);
+                frac = a;
+                frac.reduce();
+            }
+        }
     }
     return in;
 }
@@ -39,7 +61,7 @@ fraction operator+(const fraction &x,const fraction &y)
 {
     fraction result;
     int num = x.num * y.denom + y.num  * x.denom,
-        denom = x.denom * y.denom;
+            denom = x.denom * y.denom;
     result.setValue(num,denom);
     return result;
 }
@@ -48,7 +70,7 @@ fraction operator-(const fraction &x,const fraction &y)
 {
     fraction result;
     int num = x.num * y.denom - y.num  * x.denom,
-        denom = x.denom * y.denom;
+            denom = x.denom * y.denom;
     result.setValue(num,denom);
     return result;
 }
@@ -57,7 +79,7 @@ fraction operator*(const fraction &x,const fraction &y)
 {
     fraction result;
     int num = x.num * y.num,
-        denom = x.denom * y.denom;
+            denom = x.denom * y.denom;
     result.setValue(num,denom);
     return result;
 }
@@ -66,7 +88,7 @@ fraction operator/(const fraction &x,const fraction &y)
 {
     fraction result;
     int num = x.num * y.denom,
-        denom = x.denom * y.num;
+            denom = x.denom * y.num;
     result.setValue(num,denom);
     return result;
 }
@@ -74,9 +96,9 @@ fraction operator/(const fraction &x,const fraction &y)
 
 fraction operator^(const fraction &x,const fraction &y)
 {
-   double temp1 = pow(pow(x.num, y.num),1./y.denom),
-          temp2 = pow(pow(x.denom, y.num),1./y.denom);
-   return temp1/temp2;
+    double temp1 = pow(pow(x.num, y.num),1./y.denom),
+            temp2 = pow(pow(x.denom, y.num),1./y.denom);
+    return temp1/temp2;
 }
 
 
@@ -109,4 +131,3 @@ bool operator>(const fraction &x,const fraction &y)
 {
     return x.num * y.denom > y.num * x.denom;
 }
-
